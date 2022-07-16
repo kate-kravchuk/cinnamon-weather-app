29fbacf4c846a36468f6119880f3b54d707830d4
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-07-16 11:58:29 +0200

    returned an img
    returned the image source - icon from google icon

--- src/app.js
+++ src/app.js
@@ -43,6 +43,6 @@ function displayTemperature(response) {
 }
 
 let apiKey = "8cae0f906af0646fb8a7f975dc58217c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sidney&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
